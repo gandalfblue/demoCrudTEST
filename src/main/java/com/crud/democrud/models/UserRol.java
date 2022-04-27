de5ddb6014f8122a743180c70c61e0c58ea4cc6b
gandalfblue <casanova01_a@hotmail.com>
2022-04-27 16:57:46 -0500

    Creando la clase UserRol

---
+++ src/main/java/com/crud/democrud/models/UserRol.java
@@ -0,0 +1,47 @@
+package com.crud.democrud.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "userRol")
+public class UserRol implements Serializable {
+
+    private  static final Long  serialVersionUID = 1L ;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long idrol;
+
+    @Column
+    private String rol;
+
+    public UserRol() {
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = UsuarioModel.class, optional = false)
+    @JoinColumn(name = "userRol_idrol", nullable = false)
+    @JsonBackReference
+    private UsuarioModel usuario;
+
+    public Long getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(Long idrol) {
+        this.idrol = idrol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
--- src/main/java/com/crud/democrud/models/UsuarioModel.java
+++ src/main/java/com/crud/democrud/models/UsuarioModel.java
@@ -1,20 +1,47 @@
 package com.crud.democrud.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "usuario")
-public class UsuarioModel {
+public class UsuarioModel implements Serializable {
 
+    private  static final Long  serialVersionUID = 1L ;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
     private Long id;
 
+    @Column(name = "nombre", nullable = false, length = 50)
     private String nombre;
+    @Column(name = "email", nullable = false, length = 30)
     private String email;
+    @Column(name = "prioridad", nullable = false, length = 10)
     private Integer prioridad;
 
+    public UsuarioModel(String nombre, String email, Integer prioridad) {
+        this.nombre = nombre;
+        this.email = email;
+        this.prioridad = prioridad;
+    }
+
+    public UsuarioModel() {
+
+    }
+
+    @OneToMany(
+            fetch = FetchType.EAGER,
+            targetEntity = UserRol.class,
+            cascade = CascadeType.REMOVE,
+            mappedBy = "usuario"
+    )
+    @JsonManagedReference
+    private List<UserRol> userrols = new ArrayList<>();
     public void setPrioridad(Integer prioridad) {
         this.prioridad = prioridad;
     }
@@ -43,18 +70,7 @@ public class UsuarioModel {
         return email;
     }
 
-    public UsuarioModel(String nombre, String email, Integer prioridad) {
-        this.nombre = nombre;
-        this.email = email;
-        this.prioridad = prioridad;
-    }
-
-    public UsuarioModel() {
-
-    }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
 }
